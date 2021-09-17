7a873ab21a1e644edec4fae61c2318d38d854515
piever <pietro.vertechi@protonmail.com>
2021-09-17 10:48:43 +0200

    add some comments

--- examples/demo.jl
+++ examples/demo.jl
@@ -1,13 +1,15 @@
 using Flux, FluxTools
 
-m = Model((Dense(3, 5), Dense(5, 4), Dense(3, 1))) do x, (l1, l2, l3)
+# Explicit model definition
+m1 = Model((Dense(3, 5), Dense(5, 4), Dense(3, 1))) do x, (l1, l2, l3)
     x = l1(x)
     x = l2(x)
     return l3(x[1:3, :])
 end
 
-m(rand(3, 12))
+m1(rand(3, 12))
 
+# Model definition via macro. As many layers as there are `$` signs
 m = @model x begin
     y = $(Dense(2, 3))(x)
     z = y .* 1.2
